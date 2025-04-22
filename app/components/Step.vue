40e6707438e3112d2b7b95057fd24f2ab77ed52c
Jeremy Butler <jeremy@jeremymbutler.com>
2025-04-23 09:07:56 +1000

    Update instructions

---
+++ app/components/Step.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="mt-10 w-full relative z-10">
+    <div class="flex items-start gap-5">
+      <StepNumber>{{ number }}</StepNumber>
+      <div class="flex-1">
+        <h3 class="text-2xl font-semibold text-centerd">{{ title }}</h3>
+        <p v-if="description" class="mt-2 text-zinc-400">{{ description }}</p>
+        <slot></slot>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import StepNumber from "./StepNumber.vue";
+
+defineProps<{
+  number: number;
+  title: string;
+  description?: string;
+}>();
+</script>
---
+++ app/components/StepNumber.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col items-center">
+    <div
+      class="flex text-sm items-center justify-center size-6 rounded-full bg-zinc-800 mt-1"
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
