ed5bead2723d13dd3d4f4c3f910c0d90bef9a940
Jeremy Butler <jeremy@jeremymbutler.com>
2025-05-31 13:53:19 +1000

    New commit

--- demos/vue/app/components/StepNumber.vue
+++ demos/vue/app/components/StepNumber.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="flex flex-col items-center">
     <div
-      class="flex text-sm items-center justify-center size-6 rounded-full bg-zinc-800 mt-1"
+      class="flex text-xs font-semibold items-center justify-center size-6 rounded-full bg-zinc-900 border border-zinc-700 mt-1"
     >
       <slot></slot>
     </div>
   </div>
 </template>
-
-<script setup lang="ts"></script>
